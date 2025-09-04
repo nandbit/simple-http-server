4af8f5e563516ea0b0176fd889b165b6bf9d883b
Galayda.Nikita 黎軒喬 IEC1 <galayda.nikita@inventec.com>
2025-09-04 16:38:13 +0800

    finish working Socket

--- src/config.zig
+++ src/config.zig
@@ -1,13 +1,29 @@
 const std = @import("std");
 
 pub const Socket = struct {
-    address: [4]u8,
-    port: u16,
+    _address: std.net.Address,
+    _stream: std.net.Stream,
 
-    pub fn init(address: [4]u8, port: u16) Socket {
+    pub fn init(host: [4]u8, port: u16) !Socket {
+        const address = std.net.Address.initIp4(host, port); // Parses an IP address which may include a port
+        const socket = try std.posix.socket(
+            address.any.family, // domain
+            std.posix.SOCK.STREAM, // socket_type
+            std.posix.IPPROTO.TCP, // protocol
+        );
+        const stream = std.net.Stream{ .handle = socket }; // A cross-platform socket wrapper
+
         return Socket{
-            .address = address,
-            .port = port,
+            ._address = address,
+            ._stream = stream,
         };
+    }
+
+    pub fn listen(self: Socket) std.net.Address.ListenError!std.net.Server {
+        return try std.net.Address.listen(self._address, .{
+            .kernel_backlog = 128,
+            .reuse_address = false,
+            .force_nonblocking = false,
+        });
     }
 };
