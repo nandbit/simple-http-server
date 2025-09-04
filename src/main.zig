e3d629f9315b75810afad85563f05a19a4280d71
Galayda.Nikita 黎軒喬 IEC1 <galayda.nikita@inventec.com>
2025-09-04 16:38:24 +0800

    add listening

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,33 @@
 const std = @import("std");
 const Socket = @import("config.zig").Socket;
+const Request = @import("request.zig").Request;
 
 pub fn main() !void {
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
     const stdout = &stdout_writer.interface;
 
-    const address = [4]u8{ 127, 0, 0, 1 };
+    const host = [4]u8{ 127, 0, 0, 1 };
     const port: u16 = 3490;
 
-    const sock = Socket.init(address, port);
+    const sock = try Socket.init(host, port);
 
-    try stdout.print("address: {any}\n", .{sock.address});
-    try stdout.print("port: {d}\n", .{sock.port});
+    try stdout.print("Starting the server", .{});
     try stdout.flush();
+
+    var server = try sock.listen();
+    const connection = try server.accept();
+
+    var buffer: [1024]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+    var reader = connection.stream.reader(&buffer);
+
+    const interface = reader.interface();
+
+    _ = try interface.take(64);
+
+    std.debug.print("{any}\n", .{buffer});
+    std.debug.print("{any}\n", .{interface.bufferedLen()});
 }
