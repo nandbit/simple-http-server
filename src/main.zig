2148cf86921dd6d2afe517e11d191e4ac2d1a897
Galayda.Nikita 黎軒喬 IEC1 <galayda.nikita@inventec.com>
2025-09-05 14:25:59 +0800

    continue debugging hang

--- src/main.zig
+++ src/main.zig
@@ -3,38 +3,54 @@ const Socket = @import("config.zig").Socket;
 const Request = @import("request.zig").Request;
 
 pub fn main() !void {
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
-
     const host = [4]u8{ 127, 0, 0, 1 };
     const port: u16 = 3490;
 
     const sock = try Socket.init(host, port);
 
-    try stdout.print("Starting the server", .{});
-    try stdout.flush();
-
     var server = try sock.listen();
     const connection = try server.accept();
+    defer server.deinit();
+    defer connection.stream.close();
 
-    var buffer: [1024]u8 = undefined;
-    for (0..buffer.len) |i| {
-        buffer[i] = 0;
+    // Allocator
+    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    // defer arena.deinit();
+    //
+    // const allocator = arena.allocator();
+
+    // Buffers
+    var reader_buffer: [1024]u8 = undefined;
+    var writer_buffer: [1024]u8 = undefined;
+
+    for (0..reader_buffer.len) |i| {
+        reader_buffer[i] = 0;
     }
-    var reader = connection.stream.reader(&buffer);
+    for (0..writer_buffer.len) |i| {
+        writer_buffer[i] = 0;
+    }
 
-    const interface = reader.interface();
+    var reader = connection.stream.reader(&reader_buffer);
+    const reader_interface = reader.interface();
 
+    // const data = reader_interface.allocRemaining(allocator, .limited(reader_buffer.len)) catch |err| {
+    //     std.debug.print("Error: {any}\n", .{err});
+    //     return err;
+    // };
+
+    while (true) {
+        _ = reader_interface.peek(1) catch |err| {
+            std.debug.print("End of Stream: {any}\n", .{err});
+            connection.stream.close();
+        };
+    }
+    //
+    // var writer: std.Io.Writer = .fixed(&writer_buffer);
+    // const data = try reader_interface.stream(&writer, .unlimited);
+    //
+    // connection.stream.close();
+    // std.debug.print("{s}\n", .{data});
     // var read_byte: []u8 = undefined;
-    while (interface.take(1)) |byte| {
-        // do something with line...
-        std.debug.print("{any}", .{byte});
-    } else |err| switch (err) {
-        error.EndOfStream,
-        error.ReadFailed,
-        => |e| std.debug.print("{any}", .{e}),
-    }
     // while (true) {
     //     read_byte = interface.take(1) catch |err| {
     //         std.debug.print("End of stream, {any}", .{err});
